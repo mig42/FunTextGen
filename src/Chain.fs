3f8e002e070ce87f966f5bf1c802bc26305c2cfa
Miguel González <migueljgonz@gmail.com>
2020-10-11 19:38:10 +0200

    Add project
    Signed-off-by: Miguel González <migueljgonz@gmail.com>

---
+++ src/Chain.fs
@@ -0,0 +1,34 @@
+﻿namespace FunTextGen
+
+module Chain =
+    type Token = First | Last
+
+    type Word =
+        | Text of string
+        | Token of Token
+
+    type WeightedTransition = {
+        Word: Word
+        Weight: int
+    }
+
+    type Chain = Map<Word list, WeightedTransition list>
+
+    let private addTransitionToList transitions word =
+        let addWeight transition =
+            match transition with
+                | t when t.Word = word -> { t with Weight = t.Weight + 1 }
+                | _ -> transition
+
+        if List.exists (fun t -> t.Word = word) transitions then
+            List.map addWeight transitions
+        else
+            { Word = word; Weight = 1 } :: transitions
+
+    let addTransition (chain: Chain) (key: Word list) word: Chain =
+        let newTransitions =
+            match Map.tryFind key chain with
+            | Some(transitions) -> addTransitionToList transitions word
+            | None -> [{ Word = word; Weight = 1 }]
+
+        Map.add key newTransitions chain
