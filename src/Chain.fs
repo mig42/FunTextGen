152bce6d82479843733331ace48ac0fa39cc2c93
Miguel González <migueljgonz@gmail.com>
2020-10-11 20:45:38 +0200

    Move private function inside the one that consumes it
    Signed-off-by: Miguel González <migueljgonz@gmail.com>

--- src/Chain.fs
+++ src/Chain.fs
@@ -15,20 +15,20 @@ module Chain =
     type Chain = Map<Word list, WeightedTransition list>
 
     let addTransition (chain: Chain) (key: Word list) word: Chain =
+        let addTransitionToList transitions word =
+            let addWeight transition =
+                match transition with
+                    | t when t.Word = word -> { t with Weight = t.Weight + 1 }
+                    | _ -> transition
+
+            if List.exists (fun t -> t.Word = word) transitions then
+                List.map addWeight transitions
+            else
+                { Word = word; Weight = 1 } :: transitions
+
         let newTransitions =
             match Map.tryFind key chain with
             | Some(transitions) -> addTransitionToList transitions word
             | None -> [{ Word = word; Weight = 1 }]
 
         Map.add key newTransitions chain
-
-    let private addTransitionToList transitions word =
-        let addWeight transition =
-            match transition with
-                | t when t.Word = word -> { t with Weight = t.Weight + 1 }
-                | _ -> transition
-
-        if List.exists (fun t -> t.Word = word) transitions then
-            List.map addWeight transitions
-        else
-            { Word = word; Weight = 1 } :: transitions
