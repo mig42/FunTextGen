8bbd78c4512d161b3541de8580a14037311a4c3b
Miguel González <migueljgonz@gmail.com>
2020-10-13 14:24:13 +0200

    WIP implementing the next word generation
    Signed-off-by: Miguel González <migueljgonz@gmail.com>

--- src/Chain.fs
+++ src/Chain.fs
@@ -1,21 +1,24 @@
 ﻿namespace FunTextGen
 
+type Token = First | Last
+
+type Word =
+    | Text of string
+    | Token of Token
+
+type WeightedTransition = {
+    Word: Word
+    Weight: int
+}
+
+type IRandom =
+    abstract member getNext : int -> int
+
+type Chain = Map<Word list, WeightedTransition list>
+
 module Chain =
-    type Token = First | Last
-
-    type Word =
-        | Text of string
-        | Token of Token
-
-    type WeightedTransition = {
-        Word: Word
-        Weight: int
-    }
-
-    type Chain = Map<Word list, WeightedTransition list>
-
-    let addTransition (chain: Chain) (key: Word list) word: Chain =
-        let addTransitionToList transitions word =
+    module Transition =
+        let add transitions word =
             let addWeight transition =
                 match transition with
                     | t when t.Word = word -> { t with Weight = t.Weight + 1 }
@@ -26,9 +29,21 @@ module Chain =
             else
                 { Word = word; Weight = 1 } :: transitions
 
+        let getNext (random: IRandom) transitions =
+            let totalWeight = transitions |> List.sumBy (fun t -> t.Weight)
+            let targetWeight = random.getNext totalWeight
+            Token.Last
+
+    let addTransition (chain: Chain) (key: Word list) word: Chain =
         let newTransitions =
             match Map.tryFind key chain with
-            | Some(transitions) -> addTransitionToList transitions word
+            | Some(transitions) -> Transition.add transitions word
             | None -> [{ Word = word; Weight = 1 }]
 
         Map.add key newTransitions chain
+
+    let getNextWord (chain: Chain) key random =
+        match Map.tryFind key chain with
+        | Some(transitions) -> Transition.getNext random transitions
+        | None -> Token.Last
+
